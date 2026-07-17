65e2f2173d74f36c147f8b6656cc3233805d68c8
steinnzz <18421965+dstein@users.noreply.github.com>
2026-07-17 14:32:50 -0500

    Added groups to group objects and make them easier to manage

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -24,8 +30,10 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
 
